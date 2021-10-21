f73828cb82f0772fb700ba0d621be28f7966d214
Belen <maria.peirano@correo.ucu.edu.uy>
2021-10-21 19:04:01 -0300

    "me habia olvidado de pushear :)"

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,8 +6,8 @@ namespace UnitTestAndDebug
     {
         static void Main(string[] args)
         {
-            Person john = new Person("John Doe", "1.234.567-8");
-            Person jane = new Person("Jane Doe", "8.765.432-1");
+            Person john = new Person("John Doe", "1.234.567-8", "20/07/1600");
+            Person jane = new Person("Jane Doe", "8.765.432-1","20/07/1600");
             john.IntroduceYourself();
             jane.IntroduceYourself();
         }
